2ab24923da8abb4c1ce0ce43fe638a4108b37781
Heitor <heitorsmdc@gmail.com>
2025-11-13 13:43:04 -0300

    Update Maquinario.tsx

--- src/pages/Maquinario.tsx
+++ src/pages/Maquinario.tsx
@@ -2,6 +2,7 @@
 
 import { Layout } from "@/components/Layout";
 import { GerenciadorDeVeiculos } from "@/components/GerenciadorDeVeiculos";
+import { Truck } from "lucide-react"; // NOVO: Importar o ícone
 
 export default function Maquinario() {
   return (
@@ -10,6 +11,7 @@ export default function Maquinario() {
         supabaseTable="maquinario"
         title="Maquinário"
         itemNome="Maquinário"
+        icon={Truck} // NOVO: Passar o ícone como prop
       />
     </Layout>
   );
